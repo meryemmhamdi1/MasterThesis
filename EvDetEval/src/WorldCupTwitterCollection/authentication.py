f631522abaa06311c588340e90a19eeaae8c782b
Meryem M'hamdi <meryemmhamdi1@gmail.com>
2018-09-17 15:51:47 +0200

    removed api info

--- EvDetEval/src/WorldCupTwitterCollection/authentication.py
+++ EvDetEval/src/WorldCupTwitterCollection/authentication.py
@@ -1,9 +1,9 @@
 # Go to http://apps.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
-consumer_key="D76xETb7RDWb81437qX82GCno"
-consumer_secret="u4sxND6Ss4uFIJMBySGcF3H7LLFrhlIywOz8KDT36BtYD70JI4"
+consumer_key=""
+consumer_secret=""
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-access_token="765465548633309184-ceyqUnIgbkdorX2HyS7C7Pb6ti4f61U"
-access_token_secret="lp6DhzIq3n2XpY0NWh1ZVjLz6ivJkPdnZXqXhWV4udJuH"
+access_token=""
+access_token_secret=""
--- EvDetEval/src/WorldCupTwitterCollection/authentication2.py
+++ EvDetEval/src/WorldCupTwitterCollection/authentication2.py
@@ -1,9 +1,9 @@
 # Go to http://apps.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
-consumer_key="rnw0NQVlTTvfQPf7Ihkhuovb7"
-consumer_secret="EFCVfoHXWCKtshcGBQ8EN79iKV9DcbUdZbTZ3GWjiNQIZB0Dus"
+consumer_key=""
+consumer_secret=""
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-access_token="765465548633309184-IiXofw6ZOtNKuvg69NJwDdsEUntbejn"
-access_token_secret="OetvTm4HajXvyuEeRDIWiJd69eJWXFSQjMJbI4fbeUGFk"
+access_token=""
+access_token_secret=""
